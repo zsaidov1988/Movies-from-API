e103f09ce38303d72fd629be974d95b28f33efe7
zsaidov1988 <zsaidov1988@gmail.com>
2021-10-21 23:20:09 +0500

    Add new functions

---
+++ js/functions.js
@@ -0,0 +1,89 @@
+
+function fetchApi(searchText, page) {
+
+  fetch(`https://www.omdbapi.com/?s=${searchText}&apikey=2b537ff6&page=${page}`).then((response) => response.json()).then((data) => {
+    elLoadWrap.style.display = "none";
+    elOutputResult.style.display = "inline";
+    elCardsWrapper.innerHTML = "";
+    if (data.Response === "False") {
+      countResults = 0;
+      elOutputResult.classList.add("error");
+      elOutputResult.textContent = data.Error;
+      elPaginationList.style.display = "none";
+    } else {
+      countResults = parseInt(data.totalResults);
+      elOutputResult.classList.remove("error");
+      elOutputResult.textContent = `Results: ${countResults} | Page: ${pageNumber}/${Math.ceil(countResults / COUNT_MOVIE_PER_PAGE)}`;
+      elPaginationList.style.display = "flex";
+      console.log(data.Search);
+      working(data.Search);
+    }
+  });
+}
+
+function working(array) {
+  array.forEach(element => renderMovies(element))
+}
+
+function renderMovies(object) {
+
+  const cloneTemplate = elCardTemplate.cloneNode(true);
+  $_(".js-card-img", cloneTemplate).src = object.Poster;
+  $_(".js-card-title", cloneTemplate).textContent = object.Title;
+  $_(".js-movie-type", cloneTemplate).textContent = object.Type;
+  $_(".js-movie-year", cloneTemplate).textContent = object.Year;
+  $_(".js-bookmark-btn", cloneTemplate).value = object.imdbID;
+
+  elCardsWrapper.appendChild(cloneTemplate);
+}
+
+function checkPageNumber() {
+  let countpages = Math.ceil(countResults / COUNT_MOVIE_PER_PAGE);
+  if (countpages === 1) {
+    elPrevBtn.style.display = "none";
+    elNextBtm.style.display = "none";
+  } else {
+    elPrevBtn.style.display = "block";
+    elNextBtm.style.display = "block";
+    if (pageNumber === 1) {
+      elPrevBtn.style.display = "none";
+    } else if (pageNumber === countpages) {
+      elNextBtm.style.display = "none";
+    }
+  }
+}
+
+function updateBookmarkList() {
+  if (localStorage.getItem("movies")) {
+    let arrBookmarkMovies = localStorage.getItem("movies").split(",");
+    if (arrBookmarkMovies.length === 1) {
+      elBookmarkListMessage.textContent = `There are ${arrBookmarkMovies.length} movie in bookmark list`;
+    } else {
+      elBookmarkListMessage.textContent = `There are ${arrBookmarkMovies.length} movies in bookmark list`;
+    }
+    elBookmarkList.innerHTML = "";
+    arrBookmarkMovies.forEach(movie => {
+      createBookmarkItem(movie);
+    });
+  }
+  else {
+    elBookmarkListMessage.textContent = "There are not movies in bookmark list";
+    elBookmarkList.innerHTML = "";
+  }
+}
+
+function createBookmarkItem(id) {
+  fetch(`https://www.omdbapi.com/?i=${id}&apikey=2b537ff6`).then((response) => response.json()).then((data) => {
+    const elem = createElement("li", "d-flex align-items-center justify-content-between border border-secondary rounded-1 mb-1 p-1", "", elBookmarkList);
+    createElement("span", "", data.Title, elem);
+    createElement("button", "btn btn-danger btn-sm", "Delete", elem).value = data.imdbID;
+  })
+}
+
+function showFixedMessage(message) {
+  elFixedMessage.textContent = message;
+  elFixedMessage.style.visibility = "visible";
+  setTimeout(function () {
+    elFixedMessage.style.visibility = "hidden";
+  }, 1000);
+}
--- js/my.js
+++ js/my.js
@@ -1,27 +1,82 @@
 
+const COUNT_MOVIE_PER_PAGE = 10;
 
-const elLoadWrap = $_(".js-load");
-const elCardsWrapper = $_(".js-cards-wrapper");
+const elFixedMessage = $_(".js-fixed-message"); // Fixed messagebox when add movie to bookmark
+const elLoadWrap = $_(".js-load"); // Element is shown untill loading
+const elCardsWrapper = $_(".js-cards-wrapper"); // Wrapper div for movie cards
+const elSearchForm = $_(".js-search-form"); // Form for search
+const elSearchInput = $_(".js-search-input", elSearchForm); // Input for search text
+const elViewBookmarkBtn = $_(".js-view-bookmark-btn"); // View bookmark button
+const elBookmarkList = $_(".js-bookmark-list"); // Bookmark list: ul tag
+const elBookmarkListMessage = $_(".js-bookmark-massage"); // P tag for any message in bookmark list
+const elPaginationList = $_(".js-pagination-list"); // List of Pagination buttons
+const elOutputResult = $_(".js-output-result"); // Output tag to show count of results and page number and any errors
+const elPrevBtn = $_(".js-prev-btn"); // Previous button of pagination
+const elNextBtm = $_(".js-next-btn"); // Next button of pagination
 
 const elCardTemplate = $_("#js-card-template").content;
 
-fetch("http://www.omdbapi.com/?s=spider&apikey=2b537ff6").then((response) => response.json()).then((data) => {
-  console.log(data.Search);
-  elLoadWrap.style.display = "none";
-  working(data.Search)
-});
+let pageNumber = 1;
+let countResults = 0;
 
-function working(array) {
-  array.forEach(element => renderMovies(element))
-}
+elPrevBtn.style.display = "none";
+elLoadWrap.style.display = "none";
+elPaginationList.style.display = "none";
+elOutputResult.style.display = "none";
 
-function renderMovies(object) {
+updateBookmarkList();
 
-  const cloneTemplate = elCardTemplate.cloneNode(true);
-  $_(".js-card-img", cloneTemplate).src = object.Poster;
-  $_(".js-card-title", cloneTemplate).textContent = object.Title;
-  $_(".js-movie-type", cloneTemplate).textContent = object.Type;
-  $_(".js-movie-year", cloneTemplate).textContent = object.Year;
+elSearchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  pageNumber = 1;
+  elLoadWrap.style.display = "flex";
+  const searchText = elSearchInput.value.trim();
+  fetchApi(searchText, pageNumber);
+})
 
-  elCardsWrapper.appendChild(cloneTemplate);
-}
+elPrevBtn.addEventListener("click", () => {
+  pageNumber--;
+  checkPageNumber();
+  const searchText = elSearchInput.value.trim();
+  fetchApi(searchText, pageNumber);
+})
+
+elNextBtm.addEventListener("click", () => {
+  pageNumber++;
+  checkPageNumber();
+  const searchText = elSearchInput.value.trim();
+  fetchApi(searchText, pageNumber);
+})
+
+elCardsWrapper.addEventListener("click", (e) => {
+  if (e.target.className === "btn btn-primary js-bookmark-btn") {
+    let moviesInStorage = localStorage.getItem("movies") || "";
+    if (moviesInStorage === "") {
+      localStorage.setItem("movies", e.target.value);
+      showFixedMessage("Success!!!");
+      updateBookmarkList();
+    } else {
+      if (moviesInStorage.includes(e.target.value)) {
+        showFixedMessage("This movie has been already added to bookmark list");
+      } else {
+        localStorage.setItem("movies", (moviesInStorage + "," + e.target.value));
+        showFixedMessage("Success!!!");
+        updateBookmarkList();
+      }
+    }
+  }
+})
+
+elViewBookmarkBtn.addEventListener("click", () => {
+  updateBookmarkList();
+})
+
+elBookmarkList.addEventListener("click", (e) => {
+  if (e.target.className === "btn btn-danger btn-sm") {
+    let arrMoviesInLocalStor = localStorage.getItem("movies").split(",");
+    const index = arrMoviesInLocalStor.indexOf(e.target.value)
+    arrMoviesInLocalStor.splice(index, 1);
+    localStorage.setItem("movies", arrMoviesInLocalStor);
+    updateBookmarkList();
+  }
+})
